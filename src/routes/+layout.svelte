834818ed91e86172deb28c0a9adb4ead62771678
Deianne <resurrecciondeiannejeinne@gmail.com>
2024-11-03 16:26:44 +0800

    Install Google Fonts and Tailwinds

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>import "../app.css";</script>
+
+<svelte:head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet">
+</svelte:head>
+
+<slot></slot>
