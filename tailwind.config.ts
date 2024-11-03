398844ffbf7af91e0512c47aaf36d34561720920
Deianne <resurrecciondeiannejeinne@gmail.com>
2024-11-03 17:14:46 +0800

    portfolio

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,31 @@
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
+import flowbitePlugin from "flowbite/plugin";
 
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-
-  theme: {
-    extend: {
-      fontFamily: {
+	content: ["./src/**/*.{html,js,svelte,ts}", "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
+	darkMode: "selector",
+	theme: {
+		extend: {
+			fontFamily: {
 				sans: ["Yusei Magic", ...defaultTheme.fontFamily.sans],
 			},
-    }
+
+      colors: {
+        primary: {
+          50: '#FFF5F2', 
+          100: '#FFF1EE', 
+          200: '#FFE4DE', 
+          300: '#FFD5CC', 
+          400: '#FFBCAD', 
+          500: '#FE795D', 
+          600: '#EF562F', 
+          700: '#EB4F27', 
+          800: '#CC4522', 
+          900: '#A5371B'
+        },
+      },
+    },
   },
 
   plugins: []
